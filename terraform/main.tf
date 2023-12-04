87ebcd60472d9c138590dbffc04b45eb06ec61ab
gnidoc327 <gnidoc327@gmail.com>
2023-12-05 04:19:11 +0900

    Init project

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.29.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+  profile = "serverless"
+}

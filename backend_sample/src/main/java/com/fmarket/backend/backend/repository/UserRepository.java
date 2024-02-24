77f2e74f338695ff14a9600f7877190d9749b491
gnidoc327 <gnidoc327@gmail.com>
2024-02-24 15:31:20 +0900

    Rename backend directory

--- backend_sample/src/main/java/com/fmarket/backend/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fmarket.backend.backend.repository;
-
-import com.fmarket.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
